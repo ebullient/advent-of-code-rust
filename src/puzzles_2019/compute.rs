22c09608b845ddc90848c33911754e1176302acb
Erin Schnabel <ebullient@redhat.com>
2020-11-02 23:13:58 -0500

    code cleanup; move tests

--- src/puzzles_2019/compute.rs
+++ src/puzzles_2019/compute.rs
@@ -1,39 +1,32 @@
 fn opcode_1(i: usize, codes: &mut Vec<i32>) -> usize {
+    // Add value from ix + value from iy, place in iz
     let ix: usize = codes[i+1] as usize;
     let iy: usize = codes[i+2] as usize;
     let iz: usize = codes[i+3] as usize;
-    let x: i32 = codes[ix];
-    let y: i32 = codes[iy];
-    let z: i32 = x + y;
-//    println!("[ {0}] ADD value from {1}({4}) by value from {2}({5}), place in {3} ({6})", codes[i], ix, iy, iz, x, y, z);
-    codes[iz] = z;
+
+    codes[iz] = codes[ix] + codes[iy];
 
     i+4 // advance 4: 1 opcode + 3 parameters
 }
 
 fn opcode_2(i: usize, codes: &mut Vec<i32>) -> usize {
+    // Multiply value from ix * value from iy, place in iz
     let ix: usize = codes[i+1] as usize;
     let iy: usize = codes[i+2] as usize;
     let iz: usize = codes[i+3] as usize;
-    let x: i32 = codes[ix];
-    let y: i32 = codes[iy];
-    let z: i32 = x * y;
-
-//    println!("[ {0}] MULTIPLY value from {1}({4}) by value from {2}({5}), place in {3} ({6})", codes[i], ix, iy, iz, x, y, z);
-    codes[iz] = z;
+ 
+    codes[iz] = codes[ix] * codes[iy];
 
     i+4 // advance 4: 1 opcode + 3 parameters
 }
 
 pub fn run(codes: &mut Vec<i32>) {
     let mut i: usize = 0;
-//    println!("BEGIN:\n{:?}", codes);
     loop {
         match codes[i] {
             1 => { i = opcode_1(i, codes) },
             2 => { i = opcode_2(i, codes) },
             99 => { 
-//                println!("[99] Finished"); 
                 break; 
             },
             _ => {
@@ -42,5 +35,27 @@ pub fn run(codes: &mut Vec<i32>) {
             }
         }
     }
-//    println!("END: \n{:?}", codes);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use itertools::Itertools;
+
+    fn intcode_program(input_ref: &str) -> String {
+        let mut codes: Vec<i32> = input_ref.split(',')
+                                           .map(|x| x.parse::<i32>().unwrap())
+                                           .collect();
+        run(&mut codes);
+        codes.iter().join(",")
+    }
+
+    #[test]
+    fn test_intcode_program() {
+        assert_eq!(intcode_program("1,0,0,0,99"), "2,0,0,0,99");
+        assert_eq!(intcode_program("2,3,0,3,99"), "2,3,0,6,99");
+        assert_eq!(intcode_program("2,4,4,5,99,0"), "2,4,4,5,99,9801");
+        assert_eq!(intcode_program("1,1,1,4,99,5,6,0,99"), "30,1,1,4,2,5,6,0,99");
+        assert_eq!(intcode_program("1,9,10,3,2,3,11,0,99,30,40,50"), "3500,9,10,70,2,3,11,0,99,30,40,50");
+    }
 }
--- src/puzzles_2019/day_02.rs
+++ src/puzzles_2019/day_02.rs
@@ -14,10 +14,8 @@ fn run_program(input: &str, noun: i32, verb: i32) -> i32 {
                                 .collect();
     codes[1] = noun;
     codes[2] = verb;
-
     // What value is left at position 0 after the program halts?
     compute::run(&mut codes);
-
     codes[0]
 }
 
@@ -42,36 +40,10 @@ fn part_2() {
             for verb in 0..=99 {
                 let result: i32 = run_program(trim, noun, verb);
                 if result == target {
-                    println!("{0} and {1} make {2}", noun, verb, result);
                     println!("** Part 2 Final: {0}", 100 * noun + verb);
                     break 'outer;
                 }
             }
         }
-    }
-}
-
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use itertools::Itertools;
-
-    fn intcode_program(input_ref: &str) -> String {
-        let mut codes: Vec<i32> = input_ref.split(',')
-                                           .map(|x| x.parse::<i32>().unwrap())
-                                           .collect();
-        compute::run(&mut codes);
-        codes.iter().join(",")
-    }
-
-    #[test]
-    fn test_intcode_program() {
-        assert_eq!(intcode_program("1,0,0,0,99"), "2,0,0,0,99");
-        assert_eq!(intcode_program("2,3,0,3,99"), "2,3,0,6,99");
-        assert_eq!(intcode_program("2,4,4,5,99,0"), "2,4,4,5,99,9801");
-        assert_eq!(intcode_program("1,1,1,4,99,5,6,0,99"), "30,1,1,4,2,5,6,0,99");
-        assert_eq!(intcode_program("1,9,10,3,2,3,11,0,99,30,40,50"), "3500,9,10,70,2,3,11,0,99,30,40,50");
     }
 }
