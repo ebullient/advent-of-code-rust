cad028e073674c6e875c7d522cfcdeeee8fc9086
Erin Schnabel <ebullient@redhat.com>
2020-11-09 10:02:13 -0500

    day 5 part 2

--- src/puzzles_2019/day_05.rs
+++ src/puzzles_2019/day_05.rs
@@ -1,13 +1,25 @@
 use super::compute;
 
 pub fn run() {
+
     if let Ok(input) = super::read_string("./input/2019-d05-input1.txt") {
         let mut codes: Vec<i32> = input.trim().split(',')
-                                    .map(|x| x.parse::<i32>().unwrap())
+                                    .map(|x| x.trim().parse::<i32>().unwrap())
                                     .collect();
 
-                                    // What value is left at position 0 after the program halts?
-        compute::run(&mut codes);
+        let mut io = compute::ProgramIO{input: 1, output: 0};
+        compute::run(&mut codes, &mut io);
+        println!("** Part 1 Final: {:?}", io);
+    }
+
+    if let Ok(input) = super::read_string("./input/2019-d05-input1.txt") {
+        let mut codes: Vec<i32> = input.trim().split(',')
+                                    .map(|x| x.trim().parse::<i32>().unwrap())
+                                    .collect();
+
+        let mut io = compute::ProgramIO{input: 5, output: 0};
+        compute::run(&mut codes, &mut io);
+        println!("** Part 2 Final: {:?}", io);
     }
 }
 
