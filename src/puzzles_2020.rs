5c0712b3bc985fdf65e21f8f2571e389fcd0616a
Erin Schnabel <ebullient@redhat.com>
2020-12-02 12:04:30 -0500

    2020 day 2

--- src/puzzles_2020.rs
+++ src/puzzles_2020.rs
@@ -1,9 +1,11 @@
 
 mod day_01;
+mod day_02;
 
 pub fn run(day: i32) {
     match day {
         1 => day_01::run(),
+        2 => day_02::run(),
 
         // Handle the rest of cases
         _ => println!("Nothing to see here"),
