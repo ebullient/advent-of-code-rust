04d00f1d3724b45e0c8677adae85891ecdae429c
Erin Schnabel <ebullient@redhat.com>
2020-12-16 11:42:10 -0600

    2020 day 14

--- src/puzzles_2020.rs
+++ src/puzzles_2020.rs
@@ -11,6 +11,9 @@ mod day_10;
 mod day_11;
 mod day_12;
 mod day_13;
+mod day_14;
+mod day_15;
+mod day_16;
 
 pub fn run(day: i32) {
     match day {
@@ -27,6 +30,9 @@ pub fn run(day: i32) {
         11 => day_11::run(),
         12 => day_12::run(),
         13 => day_13::run(),
+        14 => day_14::run(),
+        15 => day_15::run(),
+        16 => day_16::run(),
 
         // Handle the rest of cases
         _ => println!("Nothing to see here"),
