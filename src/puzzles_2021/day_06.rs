e172d06c82db42dc8d512fd78f36024f8d84dc5e
Erin Schnabel <ebullient@redhat.com>
2021-12-07 07:37:01 -0500

    attempt using matrix lib

--- src/puzzles_2021/day_06.rs
+++ src/puzzles_2021/day_06.rs
@@ -1,4 +1,10 @@
 use crate::puzzle_input;
+extern crate nalgebra as na;
+
+use na::{SMatrix, SVector};
+
+type Vector9f = SVector<f64, 9>;
+type Matrix9x9f = SMatrix<f64, 9, 9>;
 
 pub fn run() {
     let input: Vec<usize> = puzzle_input::read_string("./input/2021-d06-input.txt")
@@ -9,14 +15,14 @@ pub fn run() {
     // this is a matrix math problem, but I have to .. do a bunch of stuff. So this is the dumb way
     let mut calc = Calculator::new(&input);
     for _ in 0..80 {
-        calc.iterate_the_long_way();
+        calc.iterate();
     }
 
     println!("** Part 1 Final: {:?}", calc.sum());
     assert_eq!(355386, calc.sum());
 
     for _ in 80..256 {
-        calc.iterate_the_long_way();
+        calc.iterate();
     }
     println!("** Part 2 Final: {:?}", calc.sum());
     assert_eq!(1613415325809, calc.sum());
@@ -25,13 +31,14 @@ pub fn run() {
 #[derive(Clone, Debug)]
 struct Calculator {
     a: [i64; 9],
-    b:  [i64; 9],
+    b: [i64; 9],
+    v: Vector9f,
+    dm: Matrix9x9f,
     is_a: bool
 }
 impl Calculator {
     pub fn new(input: &Vec<usize>) -> Calculator {
         let mut init = [0; 9];
-
         for x in input {
             init[*x] += 1;
         }
@@ -39,11 +46,34 @@ impl Calculator {
         Calculator {
             a: init,
             b: [0; 9],
-            is_a: true
+            is_a: true,
+            v: Vector9f::from_iterator(init.iter().map(|x| *x as f64)),
+            dm: Matrix9x9f::from_vec(vec![
+                0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+                1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+                1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         }
     }
 
-    fn iterate_the_long_way(&mut self) {
+    fn compute(&self, factor: u32) -> f64 {
+        println!("compute: {:?}: ", self.v);
+
+        let product = self.dm.pow(factor).unwrap();
+        println!("{:?}", product);
+
+        let result = product * self.v;
+        println!("{:?}", result);
+
+        result.sum()
+    }
+
+    fn iterate(&mut self) {
         let (prev, data) = if self.is_a { (&self.a, &mut self.b) } else { (&self.b, &mut self.a) };
         for i in 0..8 {
             match i {
@@ -96,32 +126,34 @@ mod tests {
     }
 
     #[test]
-    fn test_the_long_way() {
+    fn test() {
         let init = String::from("3,4,3,1,2");
         let (input, tally) = count(&init);
         let mut calc = Calculator::new(&input);
         assert_eq!(true, calc.compare(tally));
         assert_eq!(true, calc.compare([0,1,1,2,1,0,0,0,0]));
 
-        // calc.iterate_the_long_way();
+        // calc.iterate();
         // assert_eq!(true, calc.compare([1,1,2,1,0,0,0,0,0]));
 
-        // calc.iterate_the_long_way();
+        // calc.iterate();
         // assert_eq!(true, calc.compare([1,2,1,0,0,0,1,0,1]));
 
-        // calc.iterate_the_long_way();
+        // calc.iterate();
         // assert_eq!(true, calc.compare([2,1,0,0,0,1,1,1,1]));
 
         for _ in 0..18 {
-            calc.iterate_the_long_way();
+            calc.iterate();
         }
         let (_, tally) = count(&String::from("6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8"));
         assert_eq!(true, calc.compare(tally));
         assert_eq!(26, calc.sum());
+        //assert_eq!(26.0, calc.compute(18));
 
         for _ in 18..80 {
-            calc.iterate_the_long_way();
+            calc.iterate();
         }
         assert_eq!(5934, calc.sum());
+        //assert_eq!(5934.0, calc.compute(80));
     }
 }
