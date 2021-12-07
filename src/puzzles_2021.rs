cdfdc6762731cb1c1c49be2fe255831574ac8ab8
Erin Schnabel <ebullient@redhat.com>
2021-12-07 12:09:09 -0500

    Day 7

--- src/puzzles_2021.rs
+++ src/puzzles_2021.rs
@@ -4,7 +4,7 @@ mod day_03;
 mod day_04;
 mod day_05;
 mod day_06;
-// mod day_07;
+mod day_07;
 // mod day_08;
 // mod day_09;
 // mod day_10;
@@ -23,7 +23,7 @@ pub fn run(day: i32) {
         4 => day_04::run(),
         5 => day_05::run(),
         6 => day_06::run(),
-        // 7 => day_07::run(),
+        7 => day_07::run(),
         // 8 => day_08::run(),
         // 9 => day_09::run(),
         // 10 => day_10::run(),
---
+++ src/puzzles_2021/day_07.rs
@@ -0,0 +1,58 @@
+use crate::puzzle_input;
+use std::cmp;
+
+pub fn run() {
+    let mut input: Vec<i32> = puzzle_input::read_string("./input/2021-d07-input.txt")
+            .split(',')
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+
+    let (single, exp) = calculate_min_fuel(&mut input);
+    println!("** Part 1 Final: {:?}", single);
+    println!("** Part 2 Final: {:?}", exp);
+}
+
+fn calculate_min_fuel(input: &mut Vec<i32>) -> (i32, i32) {
+    input.sort_unstable();
+
+    let median = input[input.len() / 2];
+
+    // The answer to part 2 is going to be somewhere around the mean of the set.
+    let mean: f32 = input.iter().sum::<i32>() as f32 / input.len() as f32;
+    let m1: i32 = mean.floor() as i32;
+    let m2: i32 = mean.ceil() as i32;
+
+    println!("median is {:?}", median);
+    println!("mean: {:?} {:?}", m1, m2);
+
+    let single = input.iter().map(|x| (median - x).abs()).sum();
+    let (exp1, exp2) = input.iter().map(|x| {
+        let n1 = (m1 - x).abs();
+        let n2 = (m2 - x).abs();
+        ((n1 * (n1 + 1)) / 2 as i32, (n2 * (n2 + 1)) / 2 as i32)
+    }).reduce(|accum, item| {
+        (accum.0 + item.0, accum.1 + item.1)
+    }).unwrap();
+
+    // let exp = 0;
+    (single, cmp::min(exp1, exp2))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut input: Vec<i32> = "16,1,2,0,4,2,7,1,2,14"
+            .split(",")
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+
+        let (single, exp) = calculate_min_fuel(&mut input);
+        assert_eq!(37, single);
+        assert_eq!(168, exp);
+        assert_eq!(0, exp);
+
+    }
+}
