b9d08281bb775f99af49258040331f01372908dd
Erin Schnabel <ebullient@redhat.com>
2022-12-01 16:42:50 -0500

    2022 day 1

--- src/puzzles_2022/day_01.rs
+++ src/puzzles_2022/day_01.rs
@@ -1,11 +1,43 @@
 use crate::puzzle_input;
 
 pub fn run() {
-    let input: Vec<String> = puzzle_input::read_all_lines("./input/2022-d*-input.txt");
+    let input: Vec<String> = puzzle_input::read_all_lines("./input/2022-d01-input.txt");
+    let (m1, m2, m3) = count_calories(&input);
 
-    println!("** Part 1 Final: {:?}", 0);
-    println!("** Part 2 Final: {:?}", 0);
+    println!("** Part 1 Final: {:?}", m1);
+    println!("** Part 2 Final: {:?}", m1 + m2 + m3);
 }
+
+fn count_calories(input: &[String]) -> (i32, i32, i32) {
+    let mut m1 = 0;
+    let mut m2 = 0;
+    let mut m3 = 0;
+
+    let mut current = 0;
+    let mut capacity: Vec<i32> = Vec::new();
+
+    for line in input {
+        if line.is_empty() {
+            capacity.push(current);
+            if current > m1 {
+                m3 = m2;
+                m2 = m1;
+                m1 = current;
+            } else if current > m2 {
+                m3 = m2;
+                m2 = current;
+            } else if current > m3 {
+                m3 = current;
+            }
+            current = 0;
+        } else {
+            current += line.parse::<i32>().unwrap()
+        }
+    }
+
+    (m1, m2, m3)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -13,7 +45,29 @@ mod tests {
 
     #[test]
     fn test() {
-        let input: Vec<String> = puzzle_input::split_string("");
+        // Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
+        let input: Vec<String> = "1000
+            2000
+            3000
 
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+            "   .split('\n')
+                .map(|x| x.trim().to_string())
+                .collect();
+
+        let (m1, m2, m3) = count_calories(&input);
+        assert_eq!(m1, 24000);
+        assert_eq!(m2, 11000);
+        assert_eq!(m3, 10000);
+        assert_eq!(m1 + m2 + m3, 45000);
     }
 }
